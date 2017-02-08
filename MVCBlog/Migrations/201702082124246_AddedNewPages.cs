d12cc43629b6888bf4c5773cd372b3a934f07b2f
heymichaelw <heymichaelw@gmail.com>
2017-02-08 16:25:20 -0500

    Able to create new blog and see blog's title on index; have to add links to blog features on nav bar

---
+++ MVCBlog/Migrations/201702082124246_AddedNewPages.cs
@@ -0,0 +1,16 @@
+namespace MVCBlog.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedNewPages : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
